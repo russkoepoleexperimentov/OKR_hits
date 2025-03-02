8c2445b0f56d315f35370c946167ae01e30e62ff
Linberg34 <pfqxbr2208@gmail.com>
2025-03-02 22:38:59 +0700

    Welcome Page is done

---
+++ WebOkr/src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Input() title: string = '';
+}
