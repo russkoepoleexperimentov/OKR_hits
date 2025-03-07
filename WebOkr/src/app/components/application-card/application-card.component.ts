e0837b45d73bff21968f4b34e727be1020bd986b
Linberg34 <pfqxbr2208@gmail.com>
2025-03-07 22:46:19 +0700

    Application card is done

---
+++ WebOkr/src/app/components/application-card/application-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-card',
+  imports: [],
+  templateUrl: './application-card.component.html',
+  styleUrl: './application-card.component.scss'
+})
+export class ApplicationCardComponent {
+
+}
--- WebOkr/src/app/pages/main-page/main-page.component.ts
+++ WebOkr/src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FilterBlockComponent } from '../../components/filter-block/filter-block.component';
+import { ApplicationCardComponent } from '../../components/application-card/application-card.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent,FilterBlockComponent],
+  imports: [HeaderComponent,FilterBlockComponent,ApplicationCardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
